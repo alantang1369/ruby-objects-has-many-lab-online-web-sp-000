bf4530074d4f2da4f32f72300a8afd4764aeff66
Alan Tang <giabao_tang@yahoo.com>
2020-10-09 22:50:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,12 @@
 class Song 
-  attr_accessor :artist 
+  attr_accessor :artist :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end  
+  
+  def save
+    @@all 
+  end
 end  
